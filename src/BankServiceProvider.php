189073d847442c3f15134423ca58b7c3172a7755
Enas <enasasmar882@gmail.com>
2023-02-22 11:52:58 +0200

    fixes service provider

--- src/BankServiceProvider.php
+++ src/BankServiceProvider.php
@@ -2,21 +2,27 @@
 
 namespace Corals\Modules\Payment\Bank;
 
-use Illuminate\Support\ServiceProvider;
+use Corals\Foundation\Providers\BasePackageServiceProvider;
 use Corals\Settings\Facades\Modules;
 
-class BankServiceProvider extends ServiceProvider
+class BankServiceProvider extends BasePackageServiceProvider
 {
+    /**
+     * @var
+     */
     protected $defer = false;
+    /**
+     * @var
+     */
+    protected $packageCode = 'corals-payment-paypal';
 
     /**
      * Bootstrap the application events.
      *
      * @return void
      */
-    public function boot()
+    public function bootPackage()
     {
-        $this->registerModulesPackages();
     }
 
     /**
@@ -24,7 +30,7 @@ class BankServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function registerPackage()
     {
     }
 
